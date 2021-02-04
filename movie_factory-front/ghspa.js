a78f02ea04e64c0cd1dc080b14003d2d598994bd
Yegabebar <m.barbegayet@gmail.com>
2021-02-04 16:38:41 +0100

    Erreurs 404 de gh-pages bien captées, test de redirection vers page voulue

--- movie_factory-front/ghspa.js
+++ movie_factory-front/ghspa.js
@@ -24,10 +24,11 @@
 
    /* redirect all 404 trafic to index.html */
    function redirect() {
-     l.replace(l.protocol + '//' + l.hostname + (l.port ? ':' + l.port : '') + repo + '/?' +
-              (l.pathname ? 'p=' + l.pathname.replace(/&/g, '~and~').replace(repo, '') : '') +
-              (l.search ? '&q=' + l.search.slice(1).replace(/&/g, '~and~') : '') +
-              (l.hash))
+      var redirect = sessionStorage.redirect;
+      delete sessionStorage.redirect;
+      if (redirect && redirect != location.href) {
+        history.replaceState(null, null, redirect);
+      }
    }
 
    /* resolve 404 redirects into internal routes */
